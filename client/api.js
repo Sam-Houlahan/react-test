98b12deb282283bfb25a71c81a9fb7cf8e7fd6a2
keewhy <samhoulahan625@gmail.com>
2017-07-27 21:46:58 +1200

    input working

--- client/api.js
+++ client/api.js
@@ -1,1 +1,13 @@
-import request from superagent
+import request from 'superagent'
+
+export function getRepos (user, callback) {
+  request
+    .get(`https://api.github.com/users/${user}/repos`)
+    .end(function (err, res) {
+      if (err) {
+        callback(err)
+      } else {
+        callback(null, res.body)
+      }
+    })
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 
+import {getRepos} from '../api'
+
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      userName: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
   }
 
   render () {
+    console.log(this.state)
     return (
       <div>
         <h1>Test</h1>
+        <form>
+          <input type='text' name='userName' onChange={this.handleChange} />
+          <button>submit </button>
+        </form>
       </div>
     )
   }
