ecbc99844d9c705d8757cb3794b791ee6ef2fe0a
keewhy <samhoulahan625@gmail.com>
2017-07-27 23:25:47 +1200

    making progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,9 +6,34 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      userName: ''
+      userName: '',
+      repos: []
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.sortById = this.sortById.bind(this)
+  }
+
+  handleSubmit (evt) {
+    evt.preventDefault()
+    getRepos(this.state.userName, (err, res) => {
+      if (err) return console.err
+      this.setState({
+        repos: res
+      })
+    })
+  }
+
+  sortById (repos) {
+    const sortedIds = repos.map(repo => {
+      return repo.id
+    })
+    sortedIds.sort(function(a, b) {
+      return b - a
+    })
+    //  this.setState({
+    //    repos: newRepos
+    //  })
   }
 
   handleChange (evt) {
@@ -18,13 +43,33 @@ class App extends React.Component {
   }
 
   render () {
-    console.log(this.state)
+    console.log(this.sortById(this.state.repos))
     return (
       <div>
         <h1>Test</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' name='userName' onChange={this.handleChange} />
-          <button>submit </button>
+          <button>Load Repos </button>
+          <table className='repo-table'>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Created At</th>
+            </tr>
+            {this.state.repos.map((repo, i) => {
+              return (
+                <tbody key={i}>
+                  <tr>
+                    <td>{repo.id}</td>
+                    <td>{repo.name}</td>
+                    <td>{repo.description}</td>
+                    <td>{repo.created_at}</td>
+                  </tr>
+              </tbody>
+              )
+            })}
+          </table>
         </form>
       </div>
     )
