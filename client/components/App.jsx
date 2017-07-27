a5fb05500beaa9c343bba8b8d8c9e08d83d8e6d6
keewhy <samhoulahan625@gmail.com>
2017-07-28 11:15:43 +1200

    sort working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,11 +7,15 @@ class App extends React.Component {
     super(props)
     this.state = {
       userName: '',
-      repos: []
+      repos: [],
+      default: [],
+      receive: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.sortById = this.sortById.bind(this)
+    this.sortByName = this.sortByName.bind(this)
+    this.default = this.default.bind(this)
   }
 
   handleSubmit (evt) {
@@ -19,21 +23,35 @@ class App extends React.Component {
     getRepos(this.state.userName, (err, res) => {
       if (err) return console.err
       this.setState({
-        repos: res
+        repos: res,
+        default: res,
+        received: true
       })
     })
   }
 
   sortById (repos) {
-    const sortedIds = repos.map(repo => {
-      return repo.id
+    const sortedIds = repos.map(repo => repo.id)
+    sortedIds.sort((a, b) => b - a)
+    const sortedRepos = sortedIds.map(id => repos.find(repo => id === repo.id))
+    this.setState({
+      repos: sortedRepos
     })
-    sortedIds.sort(function(a, b) {
-      return b - a
+  }
+
+  sortByName (repos) {
+    const sortedNames = repos.map(repo => repo.name)
+    sortedNames.sort((a, b) => a - b)
+    const sortedRepos = sortedNames.map(name => repos.find(repo => name === repo.name))
+    this.setState({
+      repos: sortedRepos
     })
-    //  this.setState({
-    //    repos: newRepos
-    //  })
+  }
+
+  default () {
+    this.setState({
+      repos: this.state.default
+    })
   }
 
   handleChange (evt) {
@@ -43,10 +61,16 @@ class App extends React.Component {
   }
 
   render () {
-    console.log(this.sortById(this.state.repos))
     return (
       <div>
         <h1>Test</h1>
+          {this.state.received &&
+          <div className = 'buttons'>
+          <button onClick={() => this.sortById(this.state.repos)}>Sort By Id </button>
+          <button onClick={() => this.sortByName(this.state.repos)}>Sort By Name</button>
+          <button onClick={() => this.default()}>Default</button>
+          </div>
+          }
         <form onSubmit={this.handleSubmit}>
           <input type='text' name='userName' onChange={this.handleChange} />
           <button>Load Repos </button>
